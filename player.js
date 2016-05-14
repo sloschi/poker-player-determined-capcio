d7635462abd1499e7e0c79d4b1cd0462c198937b
sloschi <seb.losch@gmail.com>
2016-05-14 10:14:10 +0200

    implemented calculateBet

--- player.js
+++ player.js
@@ -4,10 +4,14 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(100);
+    bet(calculateBet(game_state.players[game_state.in_action].hole_cards, game_state.community_cards));
   },
 
   showdown: function(game_state) {
 
+  },
+  
+  calculateBet: function (ownCards, communityCards) {
+    return 100;
   }
 };

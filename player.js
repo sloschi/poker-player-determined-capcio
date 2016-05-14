1a88452e43ed370f17c06d89e36cfaee071bff6e
pad <pad@cap3.de>
2016-05-14 12:56:05 +0200

    * refactor calculatebet

--- player.js
+++ player.js
@@ -1,27 +1,14 @@
 var TAG = 'CAPCIO';
 
 var valueMap = require('./valuemap');
+var calculateBet = require('./valuemap');
 
-function calculateBet(gameState) {
-  var holeCards = gameState.players[gameState.in_action].hole_cards;
-  var communityCards = gameState.community_cards;
-
-  var cards = holeCards.concat(communityCards);
-  
-  var cardValue = cards.reduce(function (prev, next) {
-    return prev + valueMap.CARD[next.rank];
-  }, 0);
-
-  var averageCardValue = cardValue / cards.length;
-
-  return averageCardValue > 9 ? gameState.pot : gameState.current_buy_in;
-}
 
 module.exports = {
   VERSION: "never surrender",
 
   bet_request: function (game_state, bet) {
-    console.log(TAG, 'got bet request', game_state)
+    console.log(TAG, 'got bet request', game_state);
     try {
       bet(calculateBet(game_state));
     } catch (error) {
@@ -36,6 +23,6 @@ module.exports = {
   },
 
   showdown: function (game_state) {
-     console.log(TAG, 'showdown', game_state)
+     console.log(TAG, 'showdown', game_state);
   }
 };

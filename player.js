341a41dbd441cb571e680fc4d05a068d66f00ce2
sloschi <seb.losch@gmail.com>
2016-05-14 10:47:57 +0200

    never surrender

--- player.js
+++ player.js
@@ -4,12 +4,20 @@ function calculateBet(gameState) {
   var holeCards = gameState.players[gameState.in_action].hole_cards;
   var communityCards = gameState.community_cards;
 
-  return 100;
+  var cards = holeCards.concat(communityCards);
+  
+  var cardValue = cards.reduce(function (prev, next) {
+    return prev + valueMap.CARD[next];
+  }, 0);
+
+  var averageCardValue = cardValue / cards.length;
+
+  return averageCardValue > 9 ? gameState.pot : gameState.current_buy_in;
 }
 
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+  VERSION: "never surrender",
 
   bet_request: function (game_state, bet) {
     try {
